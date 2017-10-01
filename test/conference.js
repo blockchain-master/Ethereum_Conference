80b0ee1adb44a834035bd46ae0eb31162834bb1b
Wonderful <guardianofhappiness@dr.com>
2017-10-01 05:09:54 +0200

    refundTicket tested

--- test/conference.js
+++ test/conference.js
@@ -65,4 +65,23 @@ contract('Conference', function(accounts) {
 			}).catch(done);
 		}).catch(done);
 	});
+
+	it("should issue a refund by owner only", function(done) {
+		Conference.new({ from: accounts[0] }).then(
+			function(conference) {
+			var ticketPrice = web3.toWei(.05, 'ether');
+			var initialBalance = web3.eth.getBalance(conference.address).toNumber();
+			conference.buyTicket({ from: accounts[1], value: ticketPrice })
+			.then(function() {
+				var newBalance = web3.eth.getBalance(conference.address).toNumber();
+				var difference = newBalance - initialBalance;
+				assert.equal(difference, ticketPrice, "Difference should be what was sent");
+				return conference.refundTicket(accounts[1], ticketPrice, { from: accounts[0] });
+			}).then(function() {
+				var postRefundBalance = web3.eth.getBalance(conference.address).toNumber();
+				assert.equal(postRefundBalance, initialBalance, "Balance should be initial balance");
+				done();
+			}).catch(done);
+		}).catch(done);
+	});
 });
