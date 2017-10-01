24b3f449833f428282269cbee3e09b3f7d6e3493
Wonderful <guardianofhappiness@dr.com>
2017-10-01 04:21:46 +0200

    separate changeQuota test as individual test

--- test/conference.js
+++ test/conference.js
@@ -8,7 +8,29 @@ contract('Conference', function(accounts) {
 					function(quota) {
 						assert.equal(quota, 500, "Quota doesn't match!");
 					}
-				).then(function() {
+				)
+				.then(function() {
+					return conference.numRegistrants.call();
+				}).then(function(num) {
+					assert.equal(num, 0, "Registrants should be zero!");
+					return conference.organizer.call();
+				}).then(function(organizer) {
+					assert.equal(organizer, accounts[0], "Owner doesn't match!");
+					done();
+				}).catch(done);
+			}
+		).catch(done);
+	});
+
+	it("should update quota", function(done) {
+		Conference.new({ from: accounts[0] }).then(
+			function(conference) {
+				conference.quota.call().then(
+					function(quota) {
+						assert.equal(quota, 500, "Quota doesn't match!");
+					}
+				)
+				.then(function() {
 					return conference.changeQuota(300);
 				})
 				.then(function(result) {
@@ -17,14 +39,6 @@ contract('Conference', function(accounts) {
 				})
 				.then(function(quota) {
 					assert.equal(quota, 300, "New quota is not correct");
-				})
-				.then(function() {
-					return conference.numRegistrants.call();
-				}).then(function(num) {
-					assert.equal(num, 0, "Registrants should be zero!");
-					return conference.organizer.call();
-				}).then(function(organizer) {
-					assert.equal(organizer, accounts[0], "Owner doesn't match!");
 					done();
 				}).catch(done);
 			}
