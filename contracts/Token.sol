b994cd45cdc042a97e22193bfff7c8e561409936
Wonderful <guardianofhappiness@dr.com>
2017-10-06 08:33:12 +0200

    event and basic info attached

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,8 +1,16 @@
 contract MyToken {
 	mapping (address => uint256) public balanceOf;
+	string public name;
+	string public symbol;
+	uint8 public decimals;
 
-	function MyToken( uint256 initialSupply ) {
+	event Transer(address indexed from, address indexed to, uint256 value);
+
+	function MyToken( uint256 initialSupply, string tokenName, string tokenSymbol, uint8 decimalUnits ) {
 		balanceOf[msg.sender] = initialSupply;
+		name = tokenName;
+		symbol = tokenSymbol;
+		decimals = decimalUnits;
 	}
 
 	function transfer(address _to, uint256 _value) {
@@ -10,5 +18,8 @@ contract MyToken {
 		require(balanceOf[_to] + _value >= balanceOf[_to]);
 		balanceOf[msg.sender] -= _value;
 		balanceOf[_to] += _value;
+
+		/* Notify anyone listening that this transfer took place */
+		Transfer(msg.sender, _to, _value);
 	}
 }
